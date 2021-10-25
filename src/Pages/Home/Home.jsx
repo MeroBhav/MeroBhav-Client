8d0e3474eebc3e2b3e6f6e31f330ec6e88362ae7
Himitsu Fushigi <himitsu.fushigi@protonmail.com>
2021-10-25 10:02:56 +0545

    Create Post Card

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,26 +1,57 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
+import "./style.scss";
 
 // @components
-import LeftBar from '../../Component/Home/LeftBar/LeftBar'
+import LeftBar from "../../Component/Home/LeftBar/LeftBar";
+
+// @assets
+import UserImage from "../../Assets/userImage.JPG";
+
+// @packages
+import { Button } from "@mui/material";
 
 export default function Home() {
-    return (
-        <div className="home">
-            {/* @column left bar */} 
-            <div className="home__leftBar">
-                <LeftBar/>
-            </div>
+  return (
+    <div className="home">
+      {/* @column left bar */}
+      <div className="home__leftBar">
+        <LeftBar />
+      </div>
 
-            {/* @column posts container */}
-            <div className="home__postsContainer">
-                This is postbar
-            </div>
+      {/* @column posts container */}
+      <div className="home__postsContainer">
+        <h1>Home</h1>
+        <div className="home__postsContainer__createPost">
+          <Button>
+            <img src={UserImage} alt="" />
+          </Button>
 
-            {/* @column right bar */}
-            <div className="home__rightBar">
-                This is right bar
+          <div className="home__postsContainer__createPost__detailsEditor">
+            <textarea name="" placeholder="What's Happening?"></textarea>
+
+            <div className="home__postsContainer__createPost__detailsEditor__extraTools">
+              <div className="home__postsContainer__createPost__detailsEditor__extraTools__icons">
+                <Button>
+                  <i className="ri-image-line"></i>
+                </Button>
+                <Button>
+                  <i className="ri-file-gif-line"></i>
+                </Button>
+                <Button>
+                  <i className="ri-emotion-line"></i>
+                </Button>
+              </div>
+
+              <Button className="postButton">
+                  Post
+              </Button>
             </div>
+          </div>
         </div>
-    )
+      </div>
+
+      {/* @column right bar */}
+      <div className="home__rightBar">This is right bar</div>
+    </div>
+  );
 }
