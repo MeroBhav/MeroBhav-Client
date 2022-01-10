b5851447faace5394f21dbc352eda742196e0b75
Laxman Rai <lex.world@protonmail.com>
2022-01-10 15:20:49 +0545

    Added Edit Profile Page

---
+++ src/Pages/EditProfile/EditProfile.jsx
@@ -0,0 +1,11 @@
+import './styles/style.css'
+
+import React from 'react'
+
+export default function EditProfile() {
+    return (
+        <div className='editProfile__container'>
+            
+        </div>
+    )
+}
